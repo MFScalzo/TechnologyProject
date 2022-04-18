6378162a267ecb2bd891da76bd915bb010267687
Matt Scalzo <matthewfscalzo@gmail.com>
2022-04-18 16:56:21 -0400

    Fix for .gitignore For some reason the .gitignore is not ignoring all of these runtime files from vscode...

---
+++ producer/src/main/scala/ecommerce/HiveClient.scala
@@ -0,0 +1,30 @@
+package ecommerce
+
+import java.io.IOException
+import scala.util.Try
+import java.sql.Connection
+import java.sql.Statement
+import java.sql.DriverManager
+
+class HiveClient {
+    val dataBaseName = "Project2"
+    def loadCSV(filePath: String): Unit = {
+        var connection: java.sql.Connection = null
+
+        try {
+            var driverName = "org.apache.hive.jdbc.HiveDriver"
+            val connectionString = "jdbc:hive2://sandbox-hdp.hortonworks.com:10000/default"
+
+            connection = DriverManager.getConnection(connectionString, "", "")
+            val statement = connection.createStatement()
+
+            var query = s"CREATE DATABASE [IF NOT EXISTS] $dataBaseName;"
+
+        }
+        catch {
+
+        }
+    }
+}
+
+
