c4045743f86418e25af14bb8888998b2245e94b4
dtubbs174 <100701446+dtubbs174@users.noreply.github.com>
2022-04-20 12:11:53 -0400

    Update Product.scala
    Shortened product names for clarity.

--- producer/src/main/scala/ecommerce/Product.scala
+++ producer/src/main/scala/ecommerce/Product.scala
@@ -6,21 +6,21 @@ It contains one function in the Product class which returns one random product w
 class Product {
     def generateProductInfo: Any = {
         val productList = Vector(
-        ("Electronics", "iPhone 13 Pro Max", 1099.00, 1), 
-        ("Electronics", "S95B OLED 4K Smart TV (2022)", 2199.99, 2), 
-        ("Electronics", "LG Eectronics 33in. 25 cu. ft. 3-Door French Door Refridgerator in PrintProof Stainless Steel", 2099, 3),  
-        ("Electronics", "MacBook Pro 14\" Laptop - Apple M1 Pro chip - 16GB Memory - 1TB SSD (Latest ModeL) - Space Gray", 2499.00, 4),
-        ("Electronics", "Sony - PlayStation 5 Console", 499.00, 5), 
-        ("Bedding", "Nestwell Pure Earth Organic Cotton 3-Piece Full/Queen Comforter Set in White", 170.00, 6), 
-        ("Bedding", "Simply Essential™ Adjustable Memory Foam Standard/Queen Bed Pillow", 15.00, 7), 
-        ("Bedding", "Nestwell Supreme Softness Plush Full/Queen Blanket in Pebble Grey", 55.00, 8), 
-        ("Bedding", "Therapedic Waterproof Cotton Queen Mattress Pad", 99.99, 9), 
-        ("Bedding", "Brookstone Perfect 24-Inch Queen Air Mattress in Grey", 329.99, 10), 
-        ("Pet Supplies", "Royal Canin Feline Health Nutrition™ Indoor Adult Cat Food", 56.99, 11), 
-        ("Pet Supplies", "Whisker City 60-in Playbox, Ball Track with Scratcher Toys Cat Tree, Linen", 164.99, 12), 
-        ("Pet Supplies", "Chance & Friends \"Zen\" Plush Turtle", 5.00, 13), 
-        ("Pet Supplies", "Purina Pro Plan Complete Essentials Adult Dry Dog Food - High Protein, Probiotics, Lamb & Rice", 42.99, 14), 
-        ("Pet Supplies", "Top Paw Spiky Football Dog Toy - Squeaker", 2.99, 15)
+        ("Electronics", "iPhone 13 Pro Max", 1099.00f, 1), 
+        ("Electronics", "S95B OLED 4K Smart TV (2022)", 2199.99f, 2), 
+        ("Electronics", "LG Eectronics 3-Door French Door Refridgerator", 2099.00f, 3),  
+        ("Electronics", "MacBook Pro 14\" Laptop", 2499.00f, 4),
+        ("Electronics", "Sony - PlayStation 5 Console", 499.00f, 5), 
+        ("Bedding", "Organic Cotton 3-Piece Full/Queen Comforter Set", 170.00f, 6), 
+        ("Bedding", "Adjustable Memory Foam Standard/Queen Bed Pillow", 15.00f, 7), 
+        ("Bedding", "Supreme Softness Plush Full/Queen Blanket", 55.00f, 8), 
+        ("Bedding", "Therapedic Waterproof Cotton Queen Mattress Pad", 99.99f, 9), 
+        ("Bedding", "Perfect 24-Inch Queen Air Mattress", 329.99f, 10), 
+        ("Pet Supplies", "Royal Canin Indoor Adult Cat Food", 56.99f, 11), 
+        ("Pet Supplies", "Whisker City 60-in Cat Tree", 164.99f, 12), 
+        ("Pet Supplies", "Chance & Friends \"Zen\" Plush Turtle Toy", 5.00f, 13), 
+        ("Pet Supplies", "Purina Adult Dry Dog Food - High Protein", 42.99f, 14), 
+        ("Pet Supplies", "Spiky Football Dog Toy - Squeaker", 2.99f, 15)
         )
            
         val r = scala.util.Random
