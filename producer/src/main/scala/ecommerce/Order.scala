13738dfd597ca7263afa67518734c6893e154c31
Nicholas Young <no09young@gmail.com>
2022-04-20 16:54:41 -0500

    Commit after checking with sample products and website change

--- producer/src/main/scala/ecommerce/Order.scala
+++ producer/src/main/scala/ecommerce/Order.scala
@@ -2,10 +2,11 @@ package ecommerce
 
 import ecommerce.Product
 import scala.collection.mutable.ListBuffer
+//import scala.compiletime.ops.string
 
 class Order {
     val payment = ("Visa", "Master Card", "Discover", "PayPal", "ApplePay")
-    val web = ("Amazon", "BestBuy", "Walmart", "Ebay", "Etsy")
+    val web = ("www.Amazon.com", "www.BestBuy.com", "www.Walmart.com", "www.Ebay.com", "www.Etsy.com")
     var order_id = 1     
     val productGenerator = new Product()
 
@@ -13,18 +14,26 @@ class Order {
         order_id = order_id + 1
     }
 
-    def GenerateProductList(r: Int): List[(String, String, Float, Int, Int)] = {            //Creates a product a random number of times
-        var orderProducts = new ListBuffer[(String, String, Float, Int, Int)]()             //Will need to know number and kind of entries
+    // val productList = Vector(                                        //Sample Products for testing
+    //     ("Electronics", "iPhone 13 Pro Max", 1099.00, 1), 
+    //     ("Electronics", " 4K Smart TV (2022)", 2199.99, 2), 
+    //     ("Electronics", "LG Refridgerator Stainless Steel", 2099, 3),  
+    //     ("Electronics", "MacBook Pro Space Gray", 2499.00, 4),
+    //     ("Electronics", "Sony - PlayStation 5 Console", 499.00, 5))
+
+    def GenerateProductList(r: Int): List[String] = {            //Creates a product a random number of times
+        var orderProducts = new ListBuffer[String]()             //Will need to know number and kind of entries
         for(i <- 0 to r) {                                              
             var a = scala.util.Random
             var orderQTY = a.nextInt(10) + 1                //Creates random qty
-            orderProducts += (productGenerator.generateProductInfo() + orderQTY)     //Creates product + qty
+            orderProducts += (productGenerator.generateProductInfo() + orderQTY.toString)     //Creates product + qty
+            //orderProducts += (productList(i) + orderQTY.toString)           //Line used for testing
         }
         var order_Products = orderProducts.toList
         return order_Products
     }
 
-    def GenerateOrderInfo(): (List[(String, String, Float, Int, Int)], Any, Any, Int) = {           //Generation of one order at a time
+    def GenerateOrderInfo(): (List[String], Any, Any, Int) = {           //Generation of one order at a time
         var r = scala.util.Random                  //Random Number of Products
         var products_ordered = GenerateProductList(r.nextInt(5))      //Get List of Products + Qty
         var x = scala.util.Random
@@ -36,3 +45,12 @@ class Order {
         return order
     }
 }
+// object Test extends Order {                             //Code used for testing by itself
+    
+//     def main(args: Array[String]): Unit = {
+//         for(i <- 0 to 3){
+//             val order = GenerateOrderInfo()
+//             println(order)
+//         }
+//     }
+// }
