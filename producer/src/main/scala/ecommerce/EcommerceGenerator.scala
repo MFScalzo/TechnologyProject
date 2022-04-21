de2a8cea571642458e61ae8315e7682ffc7547d3
Matt Scalzo <matthewfscalzo@gmail.com>
2022-04-21 17:39:58 -0400

    Change functions

--- producer/src/main/scala/ecommerce/EcommerceGenerator.scala
+++ producer/src/main/scala/ecommerce/EcommerceGenerator.scala
@@ -6,66 +6,49 @@ import scala.util.Random
 
 import ecommerce.{Order, Customer}
 
-class EcommerceGenerator {
-
-    
+object EcommerceGenerator {
     val genOrder = new Order()
     val genCustomer = new Customer()
+    var currentTransactionID = 1
+    val failuerReason = ("reason1", "reason2", "reason4", "reason5")
+    
+    def main(args: Array[String]) {
 
-
-
-    def incTransaction_Id(): Unit = {             
-        incTransaction_Id += 1
     }
 
+    def incTransaction_Id(): Unit = {             
+        currentTransactionID += 1
+    }
 
-    def DateTime():
-
-
-
-
+    def DateTime(): String = {
+        val r =  new scala.util.Random
         val firstYear = 2000
         val lastYear = 2021
-        val r =  new scala.util.Random
-        val randomYear = firstYear + r.nextInt( (lastYear - firstYear) + 1)
+        val randomYear = firstYear + r.nextInt( (lastYear - firstYear))
 
         val firstMonth = 1
         val lastMonth = 12
-        val randomMonth = firstMonth + r.nextInt( (lastMonth - firstMonth) + 1)
+        val randomMonth = firstMonth + r.nextInt( (lastMonth - firstMonth))
 
         val firstDay = 1
         val lastDay = 30
-        val randomDay = firstDay + r.nextInt( (lastDay - firstDay) + 1)
+        val randomDay = firstDay + r.nextInt( (lastDay - firstDay))
 
         val firstHour = 0
         val lastHour = 23
-        val randomHour = firstHour + r.nextInt( (lastHour - firstHour) +1)
+        val randomHour = firstHour + r.nextInt( (lastHour - firstHour))
 
         val firstMinute = 0
         val lastMinute = 59
-        val randomMinute = firstMinute + r.nextInt( (lastMinute - firstMinute) +1)
+        val randomMinute = firstMinute + r.nextInt( (lastMinute - firstMinute))
 
-
-        println(s"$randomYear-$randomMonth-$randomDay  $randomHour:$randomMinute")
-
-
-    
-
-
-
-
- 
-
-
-    def GenerateData(): (List[(String, String, Int, Int, Char,String)]) ={
-
-    val Data = (genOrder,genCustomer,genDateTime,incTransaction_Id, paymentStatus, failuerReason) 
+        return s"$randomYear-$randomMonth-$randomDay  $randomHour:$randomMinute"
     }
-    val failuerReason = ("reason1", "reason2", "reason4", "reason5")
-    val genDateTime = new DateTime()
- val order = (products_ordered, ranPayment, ranWebsite, order_id)
 
-
-
+    def GenerateData(customerInfo: Tuple4[String, String, String, Int], orderInfo: Tuple8[String, String, Float, Int, Int, String, String, Int]): Tuple16[Int, Int, String, Int, String, String, String, Int, Float, String, String, String, String, Int, Char, String] = {
+        val Data = (orderID, customerID, customerName, productID, productCategory, paymentType, qty, price, datetime, country, city, webite, transactionID, transactionSuccess, failureReason) 
+    }
+    
+    val order = (products_ordered, ranPayment, ranWebsite, order_id)
 
 }
