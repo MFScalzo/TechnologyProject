4b19da8f5a2d40cc2f1f382a5ba04578b372918e
Matt Scalzo <matthewfscalzo@gmail.com>
2022-04-26 22:40:13 -0400

    Create highestRevenueByCountry for hive and spark
    I am still a little unsure what counts as writing the query in Hive vs
    Spark, technically they are in the same form, but I am calling them
    through the different methods...

--- consumer/src/main/scala/ecommerce/analysis2.scala
+++ consumer/src/main/scala/ecommerce/analysis2.scala
@@ -13,13 +13,32 @@ class Analysis2(spark: SparkSession, hiveStatement: Statement) {
     val sc = spark.sparkContext
     val sqlHiveContext = new org.apache.spark.sql.hive.HiveContext(sc)
 
+    val databaseName = "project2"
+    val tableName = "alchemy"
+
+    spark.sql(s"USE $databaseName")
+    hiveStatement.execute(s"USE $databaseName")
+
     def highestRevenueByCountry() {
-        val query = s"SELECT country, sum((qty * price)) from alchemy "
+        val query = s"""SELECT country, SUM(qty * price) as revenue
+                        FROM $tableName
+                        GROUP BY country
+                        ORDER BY revenue DESC LIMIT 1;""""
+
+        spark.sql(query).show()
     }
 
     def highestRevenueByCountryHive() {
-        var query = s""
-        hiveStatement.execute(query)
+        var query = s"""SELECT country, SUM(qty * price) as revenue
+                        FROM $tableName
+                        GROUP BY country
+                        ORDER BY revenue DESC LIMIT 1;""""
+
+        val result = hiveStatement.executeQuery(query)
+
+        if (result.next()) {
+            System.out.println(result.getString(1) + "\t" + result.getString(2));
+        }
     }
 
     def mostPopularDay() {
