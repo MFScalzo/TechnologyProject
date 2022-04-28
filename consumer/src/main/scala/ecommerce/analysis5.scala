4029f1c45e67f43ee651b69a2f026df0299b5748
Nicholas Young <no09young@gmail.com>
2022-04-28 17:59:09 -0500

    Formatted output and added percent calculations

--- consumer/src/main/scala/ecommerce/analysis5.scala
+++ consumer/src/main/scala/ecommerce/analysis5.scala
@@ -27,8 +27,14 @@ class Analysis5(spark: SparkSession, hiveStatement: Statement, dataframe: DataFr
                                 GROUP BY payment_txn_success
                                 ORDER BY occurrences""")
         
-        println("Finding the number of failures and successes...")
+        println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nFinding the number of failures and successes...\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
         df2.show(2)
+        val x = df2.select("occurrences").where("payment_txn_success == F")
+        val fail: Int = x.first().getInt(1)
+        val y = df2.select("occurrences").where("payment_txn_success == Y")
+        val pass: Int = y.first().getInt(1)
+        val percent: Float = (fail / (pass + fail))*100
+        println(s"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nPayments failed ${percent%.2f}% of the time")
        
     }
     
@@ -38,13 +44,23 @@ class Analysis5(spark: SparkSession, hiveStatement: Statement, dataframe: DataFr
                     FROM $table
                     GROUP BY payment_txn_success"""
         
-        println("Finding number of transaction failures and successes...")
-        var result = hiveStatement.executeQuery(query)
+        println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nFinding number of transaction failures and successes...\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        var result = hiveStatement.executeQuery(query)      
+        var fail = 0
+        var pass = 0
+        println(s"Success(Y/N)\tOccurrences\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
         while(result.next()){
-            if(result.getString(1) == 'N' || result.getString(1) == 'Y'){
-                System.out.println(result.getString(1) + "\t" + result.getString(2))
+            if(result.getString(1) == 'N'){ //|| result.getString(1) == 'Y'){
+                System.out.println(s"${result.getString(1)}\t\t${result.getString(2)}")
+                fail = result.getInt(2)
+            }
+            else if(result.getString(1) == 'Y'){
+                System.out.println(f"${result.getString(1)}\t\t${result.getString(2)}")
+                pass = result.getInt(2)
             }
         }
+        val percent: Float = (fail /(pass + fail))*100
+        println(s"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nPayments failed ${percent%.2f}% of the time.")
     }
 
     def commonPaymentFail(): Unit = {
@@ -56,7 +72,7 @@ class Analysis5(spark: SparkSession, hiveStatement: Statement, dataframe: DataFr
                           GROUP BY failure_reason
                           ORDER BY occurrences DESC LIMIT 1""")
         
-        println("Finding most common reason for payment failure...")
+        println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nFinding most common reason for payment failure...\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
         df2.show()
         
     }
@@ -69,10 +85,10 @@ class Analysis5(spark: SparkSession, hiveStatement: Statement, dataframe: DataFr
                           GROUP BY failure_reason
                           ORDER BY occurrences DESC LIMIT 1"""		
 
-        println("Finding most common reason for payment failure...")
+        println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nFinding most common reason for payment failure...\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
         var listReasons = hiveStatement.executeQuery(queryList)
         if(listReasons.next()){
-            System.out.println(listReasons.getString(1) + "\t" + listReasons.getString(2))
+            System.out.println(s"Reason for Failure\tOccurrences\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n${listReasons.getString(1)}\t\t${listReasons.getString(2)}")
         }
     }
 
