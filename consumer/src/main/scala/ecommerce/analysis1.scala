d7575c9bee2a69fdb1af47cf0a45c5966cfccdee
dtubbs174 <100701446+dtubbs174@users.noreply.github.com>
2022-04-28 08:23:19 -0400

    Updated queries after testing on other team's csv

--- consumer/src/main/scala/ecommerce/analysis1.scala
+++ consumer/src/main/scala/ecommerce/analysis1.scala
@@ -13,13 +13,13 @@ class Analysis1(spark: SparkSession, hiveStatement: Statement) {
     val sc = spark.sparkContext
     val sqlHiveContext = new org.apache.spark.sql.hive.HiveContext(sc)
 
-    val databaseName = "project2"
+    val databaseName = "ecommerce"
     val tableName = "alchemy"
 
     sqlHiveContext.sql(s"USE $databaseName")
     hiveStatement.execute(s"USE $databaseName")
 
-    def mostPopularProductByCategory() {
+    def mostPopularProductByCategory() {        // displays the most popoular product name and which category it belongs to with the quantity that was sold.
         val query = s"""
         SELECT product_category, product_name, SUM(qty) as quantitySold
         FROM $tableName
@@ -29,7 +29,7 @@ class Analysis1(spark: SparkSession, hiveStatement: Statement) {
         sqlHiveContext.sql(query).show()
     }
     
-    def mostPopularProductByCategoryHive() {
+    def mostPopularProductByCategoryHive() {    // Using Hive: displays the most popular product and which category it belongs to with the quantity that was sold.
         val query = s"""
         SELECT product_category, product_name, SUM(qty) as quantitySold
         FROM $tableName
@@ -42,31 +42,28 @@ class Analysis1(spark: SparkSession, hiveStatement: Statement) {
             System.out.println(result.getString(1) + "\t" + result.getString(2));
         }
     }
-    
 
-   
-    }
-
-    def highestRevenueProductByCategory(){
-        val query = s"SELECT product_category, SUM(qty * price) as revenue FROM $tableName GROUP BY product_category ORDER BY revenue DESC LIMIT 1;"
+    def highestRevenueProductByCategory(){      // displays the product and its category that has the highest revenue and that revenue.
+        val query = s"SELECT product_category, product_name, SUM(qty * price) as revenue FROM $tableName GROUP BY product_category, product name ORDER BY revenue DESC LIMIT 1;"
         sqlHiveContext.sql(query).show()
     }
         
-    def highestRevenueProductByCategoryHive() {
-        val query = s"SELECT product_category, SUM(qty * price) as revenue FROM $tableName GROUP BY product_category ORDER BY revenue DESC LIMIT 1;"
+    def highestRevenueProductByCategoryHive() { // Using Hive: displays the product and its category that has the highest revenue and that revenue.
+        val query = s"SELECT product_category, product_name, SUM(qty * price) as revenue FROM $tableName GROUP BY product_category, product name ORDER BY revenue DESC LIMIT 1;"
         val result = hiveStatement.executeQuery(query)
         
         if (result.next()) {
             System.out.println(result.getString(1) + "\t" + result.getString(2));
         } 
         
-    def highestRevenueProductByMonth() {
-        val query = s"SELECT datetime(1), SUM(qty * price) as revenue FROM $tableName GROUP BY datetime(1) ORDER BY revenue DESC LIMIT 1;"
+    def highestRevenueProductByMonth() {        //NOT WORKING: I believe it is because the datetime column is named as such, it is not recognized as a valid column here.......
+                                                // displays the highest revenue product and the month in which it sold.
+        val query = s"SELECT DATEPART(MM, datetime) as month, product_name, SUM(qty * price) as revenue FROM $tableName GROUP BY month, product_name ORDER BY revenue DESC LIMIT 1;"
         sqlHiveContext.sql(query).show()
     }
 
-    def highestRevenueProductByMonthHive() {
-        val query = s"SELECT datetime(1), SUM(qty * price) as revenue FROM $tableName GROUP BY datetime(1) ORDER BY revenue DESC LIMIT 1;"
+    def highestRevenueProductByMonthHive() {    // NOT WORKING: see above.             Using Hive: displays the highest revenue product and the month in which it sold.
+        val query =s"SELECT DATEPART(MM, datetime) as month, product_name, SUM(qty * price) as revenue FROM $tableName GROUP BY month, product_name ORDER BY revenue DESC LIMIT 1;"
         val result = hiveStatement.executeQuery(query)
         if (result.next()) {
             System.out.println(result.getString(1) + "\t" + result.getString(2));
