606477e6ae4694ac172d0b13d989e8b5b14f60db
Matt Scalzo <matthewfscalzo@gmail.com>
2022-04-28 15:23:47 -0400

    Fix output loop

--- consumer/src/main/scala/ecommerce/analysis5.scala
+++ consumer/src/main/scala/ecommerce/analysis5.scala
@@ -13,46 +13,32 @@ class Analysis5(spark: SparkSession, hiveStatement: Statement){
     val sc = spark.sparkContext
     val sqlHiveContext = new org.apache.spark.sql.hive.HiveContext(sc)
     
-    val db = "Ecommerce"
+    val db = "ecommerce"
     val table = "alchemy"
 
     sqlHiveContext.sql(s"USE $db")
     hiveStatement.execute(s"USE $db")
 
     def paymentFailPercent(): Unit = {
-        //Count Number of TXN that Pass and Fail
-        var query = s"""SELECT payment_txn_success, COUNT(payment_txn_success) 
-                    FROM $table
-                    GROUP BY payment_txn_success"""
-
-        println("Finding number of transaction failures and successes...")
-        sqlHiveContext.sql(query).show()
+        println("Spark Stuff")
     }
     
     def paymentFailPercentHive(): Unit = {
         //Count Number of TXN that Pass and Fail
-        var query = s"""SELECT payment_txn_success, COUNT(payment_txn_success) 
+        var query = s"""SELECT payment_txn_success, COUNT(payment_txn_success)
                     FROM $table
                     GROUP BY payment_txn_success"""
         
         println("Finding number of transaction failures and successes...")
         var result = hiveStatement.executeQuery(query)
-        if(result.next()){
+        while (result.next()){
             System.out.println(result.getString(1) + "\t" + result.getString(2))
         }
     
     }
 
     def commonPaymentFail(): Unit = {
-        //Count each unique failure reason and order by desc (ignores blank values due to successful txn)
-        var queryList = s"""SELECT failure_reason, COUNT(failure_reason) AS occurrences
-                          FROM $table
-                          WHERE NOT failure_reason = ""
-                          GROUP BY failure_reason
-                          ORDER BY occurrences DESC LIMIT 1"""		
-
-        println("Finding most common reason for payment failure...")
-        sqlHiveContext.sql(queryList).show()
+        println("spark stuff")
         
     }
 
