cafbe50810783bc30bad1f483722469c36090fad
Nicholas Young <no09young@gmail.com>
2022-04-28 15:23:49 -0500

    Fixed Spark Queries (Not Tested)

--- consumer/src/main/scala/ecommerce/analysis5.scala
+++ consumer/src/main/scala/ecommerce/analysis5.scala
@@ -1,6 +1,7 @@
 package ecommerce
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.types._
 
 import java.io.IOException
@@ -9,36 +10,49 @@ import java.sql.Connection
 import java.sql.Statement
 import java.sql.DriverManager
 
-class Analysis5(spark: SparkSession, hiveStatement: Statement){
+class Analysis5(spark: SparkSession, hiveStatement: Statement, dataframe: DataFrame){
     val sc = spark.sparkContext
     val sqlHiveContext = new org.apache.spark.sql.hive.HiveContext(sc)
     
     val db = "ecommerce"
     val table = "alchemy"
-
     sqlHiveContext.sql(s"USE $db")
     hiveStatement.execute(s"USE $db")
 
     def paymentFailPercent(): Unit = {
-        println("Spark Stuff")
+        //Count Number of TXN that Pass and Fail
+        dataframe.createOrReplaceTempView("FailPercent")
+        val df2 = spark.sql(s"""SELECT payment_txn_success, COUNT(payment_txn_success) AS occurrences
+                                FROM $table
+                                GROUP BY payment_txn_success
+                                ORDER BY occurrences""")
+        df2.show(2)
     }
     
     def paymentFailPercentHive(): Unit = {
         //Count Number of TXN that Pass and Fail
-        var query = s"""SELECT payment_txn_success, COUNT(payment_txn_success)
+        var query = s"""SELECT payment_txn_success, COUNT(payment_txn_success) 
                     FROM $table
                     GROUP BY payment_txn_success"""
         
         println("Finding number of transaction failures and successes...")
         var result = hiveStatement.executeQuery(query)
-        while (result.next()){
-            System.out.println(result.getString(1) + "\t" + result.getString(2))
+        while(result.next()){
+            if(result.getString(1) == 'N' || result.getString(1) == 'Y'){
+                System.out.println(result.getString(1) + "\t" + result.getString(2))
+            }
         }
-    
     }
 
     def commonPaymentFail(): Unit = {
-        println("spark stuff")
+        //Count each unique failure reason and order by desc (ignores blank values due to successful txn)
+        dataframe.createOrReplaceTempView("FailReason")
+        val df2 = spark.sql(s"""SELECT failure_reason, COUNT(failure_reason) as occurrences
+                          FROM $table
+                          WHERE NOT failure_reason = ""
+                          GROUP BY failure_reason
+                          ORDER BY occurrences DESC LIMIT 1""")
+        df2.show()
         
     }
 
